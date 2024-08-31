9f2185a45f029a364fcae6fa7277381989487de8
prakhar-radhakrishnan <96592788+prakhar-radhakrishnan@users.noreply.github.com>
2024-09-01 02:57:30 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
+ const express = require('express');
 const app = express();
-const port = 5000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.post('/message', (req, res) => {
-  const { message } = req.body;
-  res.json({ message });
+app.use("/", (req,res)=>{
+  res.send("Servre is running")
 });
+app.listen (5000, console.log("Server started on port 5000"));
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
